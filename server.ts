e8432f0bdf0482f0369b9eee065a4b1149bb0ab0
ccvv147mm-blip <ccvv147mm@gmail.com>
2026-08-08 01:54:45 +0300

    Remove unused imports for error handling
    Removed unused error handling imports from server.ts

--- server.ts
+++ server.ts
@@ -1,8 +1,3 @@
-import "./lib/error-capture";
-
-import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
-
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
 };
